584ef8ac9e6f8f24fb1f4bb54003f4f7fcb4af23
Uberi <azhang9@gmail.com>
2015-09-19 14:37:36 -0400

    Stuff.

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python3
 
+import speech_recognition as sr
+
 import pygame
 
 width, height = 800, 600
+HOVER_THRESHOLD = 100
+
+# set up the keys array
 KEYS = {}
 for i, letter in enumerate("qwertyuiop"): KEYS[letter] = pygame.Rect(50 * i + 10, 10, 50, 50)
 for i, letter in enumerate("asdfghjkl"): KEYS[letter] = pygame.Rect(50 * i + 30, 60, 50, 50)
 for i, letter in enumerate("zxcvbnm"): KEYS[letter] = pygame.Rect(50 * i + 50, 110, 50, 50)
 
-#wip: represent the streak as a sequence of lists of pairs containing letters and the distances from the center of their rectangle
+# wip: each letter should have an associated weight to it that tells us how likely it is that the user meant to actually type that letter
+# wip: need a key model that takes into account the path of the swipe, like corners or loops
 
-current_streak = []
+# load dictionary
+DICTIONARY = set()
+with open("wordlist.txt", "r") as f:
+    for word in f: DICTIONARY.add(word)
+
+def callback(recognizer, audio):
+    try:
+        print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError:
+        print("Could not request results from Google Speech Recognition service")
+r = sr.Recognizer()
+m = sr.Microphone()
+with m as source: r.adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
+stop_listening = r.listen_in_background(m, callback)
+
+current_swipe = []
+def process_swipe(swipe):
+    print(swipe)
 
 screen = pygame.display.set_mode((width, height))
-
 pygame.font.init()
 font = pygame.font.SysFont("monospace", 18)
 def draw_keyboard(pressed_keys):
@@ -28,8 +52,7 @@ def draw_keyboard(pressed_keys):
 
 while True:
     event = pygame.event.poll()
-    if event.type == pygame.QUIT:
-        break
+    if event.type == pygame.QUIT: break
 
     screen.fill((0, 0, 0))
 
@@ -39,13 +62,17 @@ while True:
         current_pressed = set()
         for key, rectangle in KEYS.items():
             if rectangle.collidepoint(pos):
-                current_streak.append(key)
-                current_pressed = {key}
+                distance_from_key_center = ((rectangle.centerx - pos[0]) ** 2 + (rectangle.centery - pos[1]) ** 2) ** 0.5
 
+                if not current_swipe or current_swipe[-1] != key:
+                    current_swipe.append(key)
+                current_pressed = {key}
+                break
 
         draw_keyboard(current_pressed)
     else:
-        current_streak.clear()
+        if current_swipe: process_swipe(current_swipe)
+        current_swipe.clear()
 
         draw_keyboard(set())
     pygame.display.flip()
