80d7683fad1f5d7d1defbfb5318f3417049f7ae6
Uberi <azhang9@gmail.com>
2015-09-19 20:19:59 -0400

    More.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict
+import math
 
 import speech_recognition as sr
 import pygame
 
-width, height = 600, 300
+width, height = 600, 400
 HOVER_THRESHOLD = 100
 
 # set up the keys array
@@ -16,27 +17,41 @@ for i, letter in enumerate("zxcvbnm"): KEYS[letter] = pygame.Rect(50 * i + 50, 1
 
 # wip: each letter should have an associated weight to it that tells us how likely it is that the user meant to actually type that letter
 # wip: need a key model that takes into account the path of the swipe, like corners or loops
+# good for demo: polo, ward, red, loom, kite, two, fear, how, the, test
+
+CURRENT_CHOICES = {}
+CURRENT_DISPLAYED = ""
 
 # load dictionary
 DICTIONARY = defaultdict(list)
-with open("wordlist.txt", "rb") as f:
-    for line in f:
-        word = line.decode("utf-8", errors="replace").rstrip("\r\n")
-        no_dup_word = "".join(letter for i, letter in enumerate(word) if i == 0 or word[i - 1] != letter)
-        DICTIONARY[no_dup_word].append(word)
+with open("wordlist.txt", "rb") as f: # ordered by frequency of use in English
+    for i, line in enumerate(f):
+        word = line.decode("utf-8", errors="replace").rstrip("\r\n").lower()
+        no_dup_word = "".join(letter for j, letter in enumerate(word) if j == 0 or word[j - 1] != letter)
+        DICTIONARY[no_dup_word].append((word, i))
 
-print(DICTIONARY["test"])
 def callback(recognizer, audio):
+    global CURRENT_DISPLAYED
     try:
         spoken_result = recognizer.recognize_google(audio, show_all=True)
+        if spoken_result == []: raise sr.UnknownValueError()
     except sr.UnknownValueError:
-        pass
+        return
     except sr.RequestError:
-        pass
-    possibilities = [entry["transcript"] for entry in spoken_result["alternative"]]
-    for transcript in possibilities
+        return
+    possibilities = {entry["transcript"].lower(): i for i, entry in enumerate(spoken_result["alternative"])}
+    combined_possibilities = {}
+    for possibility, rank in possibilities.items():
+        if possibility not in CURRENT_CHOICES: continue
+        combined_possibilities[possibility] = rank + 0.1 * math.log(CURRENT_CHOICES[possibility] + 1)
+    print(combined_possibilities, possibilities)
+    if not combined_possibilities: return # no candidates
+    best_candidate = max(combined_possibilities.keys(), key=len) # longest result
+    CURRENT_DISPLAYED = " ".join(CURRENT_DISPLAYED.split()[:-1] + [best_candidate])
 
 r = sr.Recognizer()
+r.dynamic_energy_threshold = False
+r.energy_threshold = 900
 m = sr.Microphone()
 with m as source: r.adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
 stop_listening = r.listen_in_background(m, callback)
@@ -55,29 +70,32 @@ def word_removals(word, depth = 0):
     for i in range(len(word)): yield from n_removals(word, i)
 
 current_swipe = []
-current_sentence = []
 def process_swipe(swipe):
+    global CURRENT_DISPLAYED
+
     # `word` is a string of characters with no runs - each character is different from the one before it
     word = "".join(swipe)
-    result = set()
+    CURRENT_CHOICES.clear()
     for word_variation in word_removals(word):
-        if word_variation == "test": print("aaaa")
         if word_variation in DICTIONARY:
-            current_result.extend(DICTIONARY[word_variation])
+            for real_word, rank in DICTIONARY[word_variation]:
+                CURRENT_CHOICES[real_word] = rank
+    best_candidate = max(CURRENT_CHOICES.items(), key=lambda x: len(x[0]) - 0.1 * math.log(x[1] + 1))[0] # arbitrary ranking function that seems to work well
+    CURRENT_DISPLAYED = " ".join(CURRENT_DISPLAYED.split() + [best_candidate])
 
 screen = pygame.display.set_mode((width, height))
 pygame.font.init()
-font = pygame.font.SysFont("monospace", 18)
+keyboard_font = pygame.font.SysFont("monospace", 18)
+display_font = pygame.font.SysFont("monospace", 20)
 def draw_keyboard(pressed_keys):
     for key, rectangle in KEYS.items():
         if key in pressed_keys:
             screen.fill((255, 255, 255), rectangle)
-            label = font.render(key, 1, (0, 0, 0))
+            label = keyboard_font.render(key, 1, (0, 0, 0))
         else:
             pygame.draw.rect(screen, (255, 255, 255), rectangle, 1)
-            label = font.render(key, 1, (255, 255, 255))
+            label = keyboard_font.render(key, 1, (255, 255, 255))
         screen.blit(label, (rectangle.left + 10, rectangle.top + 10))
-
 while True:
     event = pygame.event.poll()
     if event.type == pygame.QUIT: break
@@ -103,6 +121,10 @@ while True:
         current_swipe.clear()
 
         draw_keyboard(set())
+
+    display = display_font.render(CURRENT_DISPLAYED, 1, (255, 255, 255))
+    screen.blit(display, (10, 200))
+
     pygame.display.flip()
 
 pygame.quit()
