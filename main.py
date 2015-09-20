90df5ee64908f7e8e87328efc389b52a5a0b1474
Uberi <azhang9@gmail.com>
2015-09-20 08:03:55 -0400

    Refactor

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ for i, letter in enumerate("zxcvbnm"): KEYS[letter] = pygame.Rect(50 * i + 50, 1
 # wip: need a key model that takes into account the path of the swipe, like corners or loops
 # good for demo: polo, ward, red, loom, kite, two, fear, how, the, test
 
-CURRENT_CHOICES = {}
+CURRENT_SWIPE_CHOICES = {}
+CURRENT_VOICE_CHOICES = {}
 CURRENT_DISPLAYED = ""
+CURRENT_SWIPE = []
 
 # load dictionary
 DICTIONARY = defaultdict(list)
@@ -30,8 +32,19 @@ with open("wordlist.txt", "rb") as f: # ordered by frequency of use in English
         no_dup_word = "".join(letter for j, letter in enumerate(word) if j == 0 or word[j - 1] != letter)
         DICTIONARY[no_dup_word].append((word, i))
 
-def callback(recognizer, audio):
+def solve_result(voice_possibilities, swipe_possibilities):
+    combined_possibilities = {}
+    for possibility, rank in voice_possibilities.items():
+        if possibility not in CURRENT_SWIPE_CHOICES: continue
+        combined_possibilities[possibility] = rank + 0.1 * math.log(swipe_possibilities[possibility] + 1)
+    print(combined_possibilities, voice_possibilities, swipe_possibilities)
+    if not combined_possibilities: return # no candidates
+    best_candidate = max(combined_possibilities.keys(), key=len) # longest result
+
     global CURRENT_DISPLAYED
+    CURRENT_DISPLAYED = " ".join(CURRENT_DISPLAYED.split()[:-1] + [best_candidate])
+
+def process_voice(recognizer, audio):
     try:
         spoken_result = recognizer.recognize_google(audio, show_all=True)
         if spoken_result == []: raise sr.UnknownValueError()
@@ -39,22 +52,15 @@ def callback(recognizer, audio):
         return
     except sr.RequestError:
         return
-    possibilities = {entry["transcript"].lower(): i for i, entry in enumerate(spoken_result["alternative"])}
-    combined_possibilities = {}
-    for possibility, rank in possibilities.items():
-        if possibility not in CURRENT_CHOICES: continue
-        combined_possibilities[possibility] = rank + 0.1 * math.log(CURRENT_CHOICES[possibility] + 1)
-    print(combined_possibilities, possibilities)
-    if not combined_possibilities: return # no candidates
-    best_candidate = max(combined_possibilities.keys(), key=len) # longest result
-    CURRENT_DISPLAYED = " ".join(CURRENT_DISPLAYED.split()[:-1] + [best_candidate])
+    CURRENT_VOICE_CHOICES = {entry["transcript"].lower(): i for i, entry in enumerate(spoken_result["alternative"])}
+    solve_result(CURRENT_VOICE_CHOICES, CURRENT_SWIPE_CHOICES)
 
 r = sr.Recognizer()
 r.dynamic_energy_threshold = False
 r.energy_threshold = 9000
 r.phrase_threshold = 0.1
 m = sr.Microphone()
-stop_listening = r.listen_in_background(m, callback)
+stop_listening = r.listen_in_background(m, process_voice)
 
 def word_removals(word, depth = 0):
     """Return all the possible variations of `word` with each letter successively removed, from longest to shortest"""
@@ -68,21 +74,20 @@ def word_removals(word, depth = 0):
         for sequence in n_removals(word[1:], k): yield word[0] + sequence
     for i in range(len(word)): yield from n_removals(word, i)
 
-current_swipe = []
 def process_swipe(swipe):
     global CURRENT_DISPLAYED
 
     # `word` is a string of characters with no runs - each character is different from the one before it
     word = "".join(swipe)
-    CURRENT_CHOICES.clear()
+    CURRENT_SWIPE_CHOICES.clear()
     if len(word) == 1: # simple key press
-        CURRENT_CHOICES[word] = 0
+        CURRENT_SWIPE_CHOICES[word] = 0
     else:
         for word_variation in word_removals(word):
             if word_variation in DICTIONARY:
                 for real_word, rank in DICTIONARY[word_variation]:
-                    CURRENT_CHOICES[real_word] = rank
-    best_candidate = max(CURRENT_CHOICES.items(), key=lambda x: len(x[0]) - 0.1 * math.log(x[1] + 1))[0] # arbitrary ranking function that seems to work well
+                    CURRENT_SWIPE_CHOICES[real_word] = rank
+    best_candidate = max(CURRENT_SWIPE_CHOICES.items(), key=lambda x: len(x[0]) - 0.1 * math.log(x[1] + 1))[0] # arbitrary ranking function that seems to work well
     CURRENT_DISPLAYED = " ".join(CURRENT_DISPLAYED.split() + [best_candidate])
 
 screen = pygame.display.set_mode((width, height))
@@ -113,15 +118,16 @@ while True:
             if rectangle.collidepoint(pos):
                 distance_from_key_center = ((rectangle.centerx - pos[0]) ** 2 + (rectangle.centery - pos[1]) ** 2) ** 0.5
 
-                if not current_swipe or current_swipe[-1] != key:
-                    current_swipe.append(key)
+                if not CURRENT_SWIPE or CURRENT_SWIPE[-1] != key: # remove duplicate consecutive keys
+                    CURRENT_SWIPE.append(key)
                 current_pressed = {key}
                 break
 
         draw_keyboard(current_pressed)
     else:
-        if current_swipe: process_swipe(current_swipe)
-        current_swipe.clear()
+        if CURRENT_SWIPE:
+            process_swipe(CURRENT_SWIPE)
+            CURRENT_SWIPE.clear()
 
         draw_keyboard(set())
 
